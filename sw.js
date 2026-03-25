077891562d6910b3bbfbb9c6982d68e8a93eb6cd
Kalleren <sharpcutta@gmail.com>
2026-03-25 21:51:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,25 +7,21 @@ var urlsToCache = [
   './icon-512.png'
 ];
 
-// Installer: cache alle filer
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
-      console.log('Cache åbnet');
       return cache.addAll(urlsToCache);
     })
   );
   self.skipWaiting();
 });
 
-// Aktivér: ryd gamle caches
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheName !== CACHE_NAME) {
-            console.log('Sletter gammel cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -35,25 +31,19 @@ self.addEventListener('activate', function(event) {
   self.clients.claim();
 });
 
-// Fetch: server fra cache, fald tilbage til netværk
+// Network-first strategi: prøv altid netværk først, fald tilbage til cache
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(function(response) {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-        var responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
+    fetch(event.request).then(function(response) {
+      // Opdater cachen med den nye version
+      var responseToCache = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, responseToCache);
       });
+      return response;
     }).catch(function() {
-      return caches.match('./index.html');
+      // Offline: brug cached version
+      return caches.match(event.request);
     })
   );
 });
